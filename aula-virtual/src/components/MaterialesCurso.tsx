4630f743fd5cefa71ce099bad5de28512c27d039
angel <angeladrianvelasquezbarragan@gmail.com>
2025-09-19 23:56:37 -0500

    Agrega componente básico MaterialesCurso

---
+++ aula-virtual/src/components/MaterialesCurso.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MaterialesCurso: React.FC = () => {
+  return (
+    <div>
+      <h2>Materiales del Curso</h2>
+      <p>Aquí puedes agregar los materiales del curso.</p>
+    </div>
+  );
+};
+
+export default MaterialesCurso;
